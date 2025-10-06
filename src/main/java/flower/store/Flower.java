446023f1d7cfc0bbfb2619d19f65387f642eeaef
antondep <antondep007@gmail.com>
2025-10-06 12:19:30 +0300

    Added types and fixed bucket

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,11 +1,13 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 
 
 @Setter
+@AllArgsConstructor
 public class Flower {
     @Getter
     private double sepalLength;
@@ -14,6 +16,20 @@ public class Flower {
     private double price;
     @Getter
     private FlowerType flowerType;
+
+    public Flower(Flower other) {
+        this.sepalLength = other.sepalLength;
+        this.color = other.color;
+        this.price = other.price;
+        this.flowerType = other.flowerType;
+    }
+    
+    public Flower() {
+        this.sepalLength = 0;
+        this.color = FlowerColor.RED;
+        this.price = 0;
+        this.flowerType = FlowerType.CHAMOMILE;
+    }
 
     public String getColor() {
         return color.toString();
